1e102a61095eaf69e5753ad818df4e5f887a4700
Tomislav Dukez <tomdu3@ymail.com>
2024-02-15 19:35:02 +0000

    Add sort algorithms code

---
+++ lesson-02/sort.py
@@ -0,0 +1,34 @@
+numbers = [4, 5, 1,34, 100, -1, -20, 7]
+
+words = ['Ada', 'Loveless', 'atom', 'eon', 'p', '01', '007']
+
+
+# sorting numbers
+
+for i in range(len(numbers)-1):
+	for j in range(len(numbers) - i):
+		if numbers[i] > numbers[i + j]:
+			numbers[i], numbers[i + j] = numbers[i + j], numbers[i]
+
+print(numbers)
+
+
+# sorting strings - case insensitive
+# sorting strings
+
+for i in range(len(words)-1):
+	for j in range(len(words) - i):
+		if words[i] > words[i + j]:
+			words[i], words[i + j] = words[i + j], words[i]
+
+print(words)
+
+
+# sorting strings - case sensitive
+
+for i in range(len(words)-1):
+	for j in range(len(words) - i):
+		if words[i].casefold() > words[i + j].casefold():
+			words[i], words[i + j] = words[i + j], words[i]
+
+print(words)
