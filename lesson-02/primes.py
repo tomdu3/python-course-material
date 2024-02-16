16ac7f2053b5409979b256a4a233836edf76b0e1
Tomislav Dukez <tomdu3@ymail.com>
2024-02-16 09:02:06 +0000

    Add prime to lesson-02

---
+++ lesson-02/primes.py
@@ -0,0 +1,12 @@
+def prime(num):
+    for i in range(int(num ** 0.5)-1):
+        if num % (i+2) == 0:
+            return False
+    return True
+
+print(prime(7))
+print(prime(9))
+print(prime(101))
+    
+
+
