7f0d7998048e22c24be9500675728df2bbdbe949
Tomislav Dukez <tomdu3@ymail.com>
2024-03-23 11:27:53 +0000

    Add take out vowels code

---
+++ lesson-02/weekly-challenge/no-vowels.py
@@ -0,0 +1,4 @@
+# You can use a for loop or while to do this.
+# You need to ask a user to insert a word and then remove all vowels (a, e, i, o, u) from it.
+# You need to use the continue statement.
+# Print out only the characters that aren't vowels.
