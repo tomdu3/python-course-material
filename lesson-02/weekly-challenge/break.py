4a5957aef2ea09589b2ee2440205464e087e5993
Tomislav Dukez <tomdu3@ymail.com>
2024-03-23 11:27:30 +0000

    Add break out of loop code

---
+++ lesson-02/weekly-challenge/break.py
@@ -0,0 +1,4 @@
+# Make a program that asks the user to enter a word. If that word is "quit", print "Goodbye!" and exit the program, otherwise do nothing and ask for a word input again.
+# You should use break from the loop.
+
+
