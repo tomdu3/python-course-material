4738f5f65ab9bfaa3d6f1d6a49d4e26fa7498f3a
Tomislav Dukez <tomdu3@ymail.com>
2024-04-26 12:58:50 +0200

    Add class example lesson 05

---
+++ lesson-06/examples/class-example.py
@@ -0,0 +1,12 @@
+# A Simple Class
+
+class Animal:
+
+    # method - function that belongs to a class
+    def says(self):
+        return "I am an animal!"
+
+# object or instance of a class
+animal = Animal()
+
+print(animal.says())
