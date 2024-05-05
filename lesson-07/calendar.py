f69127d91e31825f20e21e7dbe084f38da6ef704
Tomislav Dukez <tomdu3@ymail.com>
2024-05-05 05:50:49 +0200

    Fix: remove calendar.py

--- lesson-07/calendar.py
+++
@@ -1,9 +0,0 @@
-# credit to: https://www.instagram.com/reel/C3fIsFALO39/?utm_source=ig_web_copy_link
-
-import calendar
-
-yy = 2024  # year
-mm = 5  # month
-
-# display the calendar
-print(calendar.month(yy, mm))
