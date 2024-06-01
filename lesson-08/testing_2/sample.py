a3dfe718735f8903a407cafdb4e440ec33cb43ea
Tomislav Dukez <tomdu3@ymail.com>
2024-06-01 13:31:36 +0200

    Add sample and test to lesson 8

---
+++ lesson-08/testing_2/sample.py
@@ -0,0 +1,14 @@
+from math import isclose
+from decimal import Decimal
+
+def check_floats_equal(a, b):
+    if isclose(a, b):
+        return True
+    else:
+        return False
+
+def check_floats_equal2(a, b):
+    return round(a, 2) == round(b, 2)
+
+print(check_floats_equal(0.1, 0.09 + 0.01))
+print(check_floats_equal2(0.1, 0.09 + 0.01))
---
+++ lesson-08/testing_2/test_sample.py
@@ -0,0 +1,12 @@
+import unittest
+import sample
+
+# test functions from sample.py
+
+class TestCheckFloatsEqual(unittest.TestCase):
+    def test_check_floats_equal(self):
+        self.assertEqual(sample.check_floats_equal(0.1, 0.099999999999999), True)
+
+class TestCheckFloatEqual2(unittest.TestCase):
+    def test_check_floats_equal2(self):
+        self.assertEqual(sample.check_floats_equal2(0.5, 0.499), True)
